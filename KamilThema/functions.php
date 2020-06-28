e8fce668587580f00bab047b1d260b36f7f5fb7f
Kamil38 <55272106+Kamil38@users.noreply.github.com>
2020-06-28 16:34:01 +0200

    Add files via upload
    Video 10 afgerond met bootstrap menu

--- KamilThema/functions.php
+++ KamilThema/functions.php
@@ -19,3 +19,11 @@ function registreer_menu() {
     register_nav_menus();
 }
 add_action( 'init', 'registreer_menu' );
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'init', 'register_navwalker' );
